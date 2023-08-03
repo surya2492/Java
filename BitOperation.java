4cc01e21e6dc94d10f57a2ffdfc9070dd33f005a
surya2492 <129266053+surya2492@users.noreply.github.com>
2023-08-03 13:14:15 +0530

    Add files via upload

---
+++ BitOperation.java
@@ -0,0 +1,35 @@
+public class BitOperation{
+    public static void main(String args[]){
+
+      /* Right Shift Zero fill a<<<1
+       int c= a&b;
+        System.out.println("a&b is" + c);
+                 c= a | b;
+        System.out.println("a&b is" + c);
+                 c= ~a;
+        System.out.println("a&b is" + c);
+        System.out.println(a>>>28);
+
+         Short Circuit Logical operators
+         
+        
+
+        
+          
+
+
+         int a=10;
+         System.out.println((a>20) && (a<30));
+         System.out.println((a>20) || (a<30));
+          Operator Precedence BDMAS
+         */ 
+        int a= 10, b=25;
+        double c= a*b+10/a;
+        System.out.println(c);
+
+
+
+         
+
+    }
+}
---
+++ inputdemo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class inputdemo {
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("what is your name ");
+
+        String name = scanner.nextLine();
+        System.out.println(" hello " +name);
+
+    
+
+    }
+    
+}
